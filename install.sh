3b0bbe58156ca5aefbb82fb91e1ada47161e4863
santatra <razafindratsimba34@gmail.com>
2024-02-09 10:35:29 +0300

    add installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 file="./alakaiky"
+config_file="config.yaml"
+config_path="$HOME/.local/state/alakaiky"
 dest="/home/$USER/.local/bin"
 flag="-v"
+
+mkdir -p "$config_path"
+
+if [ ! -e $config_file ]; then
+    echo "$config_file doesn't exist!"
+else
+    cp $config_file "$config_path"
+fi
+
 cp $flag $file "$dest"
