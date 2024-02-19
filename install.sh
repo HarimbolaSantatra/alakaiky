ced6dd1c0a95f092baf02a20dc8b77b7ced202ac
santatra <razafindratsimba34@gmail.com>
2024-02-19 15:40:15 +0300

    change config file location to ./

--- install.sh
+++ install.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 file="./alk"
-config_file="config.yaml"
-config_path="$HOME/.local/state/alakaiky"
+config_file=".alk.yaml"
 dest="/home/$USER/.local/bin"
 flag="-v"
 
-mkdir -p "$config_path"
 
 if [ ! -e $config_file ]; then
     echo "$config_file doesn't exist!"
-else
-    cp $flag $config_file "$config_path"
 fi
 
 cp $flag $file "$dest"
