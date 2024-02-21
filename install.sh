937e6544f42504e8f8089d9a649db1fbf2a9af9a
santatra <razafindratsimba34@gmail.com>
2024-02-21 13:02:04 +0300

    rename alk -> alk.py

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-file="./alk"
+file="./alk.py"
 config_file=".alk.yaml"
 dest="/home/$USER/.local/bin"
+dest_file="alk"
 flag="-v"
 
-cp $flag $file "$dest"
+cp $flag $file "$dest$dest_file"
